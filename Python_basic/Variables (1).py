2e3948b39a5503db999994dd9dcc6baa921f1a3d
Akshat <122510638+Axshat0@users.noreply.github.com>
2023-10-07 20:31:46 -1200

    Add files via upload

---
+++ Python_basic/Variables (1).py
@@ -0,0 +1,22 @@
+# Variables
+first_name = "Naruto"
+last_name = "Uzumaki"
+full_name = first_name + " " + last_name
+print(type(first_name))  # Display the data type of first_name
+print("Hello " + full_name)  # Print a greeting with the full name
+
+# int (Represents whole numbers without decimal points)
+age = 15
+age += 1  # Increment age by 1
+print(type(age))  # Display the data type of age
+print("Your age is : " + str(age))  # Print the age with a message
+
+# float (Represents numbers with decimal points or fractions)
+height = 250.5
+print(type(height))  # Display the data type of height
+print("Your height is : " + str(height) + "cm")  # Print the height with a message
+
+# boolean (Has only two values: True or False)
+human = False
+print(type(human))  # Display the data type of human
+print("You're a Human : " + str(human))  # Print human status
