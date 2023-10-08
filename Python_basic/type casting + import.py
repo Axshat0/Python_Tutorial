e759ac2761ad25e8373a4c30c42827106dce6ca9
Akshat <122510638+Axshat0@users.noreply.github.com>
2023-10-07 19:36:12 -1200

    Add files via upload

---
+++ Python_basic/type casting + import.py
@@ -0,0 +1,26 @@
+#type casting
+x = 2    #int
+y = 2.0  #float
+z = "3"  #string
+
+x = int(x)
+y = float(y)
+z = str(z)
+
+# The 'import' statement is used to include external modules or libraries in your Python code.
+import math
+pi = 3.14
+pi1 = -3.14
+x = 1
+y = 2
+z = 3
+
+print(round(pi))
+print(abs(pi1))
+print(pow(pi,2))
+print(max(x,y,z))
+print(min(x,y,z))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(math.sqrt(420))
+
