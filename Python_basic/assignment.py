dadcae74540ea3e00e094106b9e7990b61a54e20
Akshat <122510638+Axshat0@users.noreply.github.com>
2023-10-08 16:00:16 -1200

    Add files via upload

---
+++ Python_basic/assignment.py
@@ -0,0 +1,19 @@
+# Multiple assignment
+# You can also write it like this
+# name = "Me"
+# age = 15
+# attractive = True
+name, age, attractive = "Me", 15, True
+print(name)
+print(age)
+print(attractive)
+
+# Chained assignment
+# You can also write it like this
+# spongebob = 30
+# patrick = 30
+# sandy = 30
+spongebob = patrick = sandy = 30
+print(spongebob)
+print(patrick)
+print(sandy)
