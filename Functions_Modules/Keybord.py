8ec4c6502fe935b22b2770d6538573cae42d7f1d
Akshat <122510638+Axshat0@users.noreply.github.com>
2023-10-08 16:03:25 -1200

    Add files via upload

---
+++ Functions_Modules/Keybord.py
@@ -0,0 +1,13 @@
+#Keybord Arguments
+def hello(first,middle,last):
+  print("Hello "+first+" "+middle+" "+last)
+
+hello("izuku","deku","midoriya") 
+
+hello("deku","izuku","midoriya") 
+
+hello(middle="deku",first="izuku",last="midoriya") 
+
+#This assigns the value "izuku" to the first parameter of the hello function.
+#This assigns the value "deku" to the middle parameter of the hello function.
+#This assigns the value "midoriya" to the last parameter of the hello function.
